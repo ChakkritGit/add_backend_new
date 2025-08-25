492755ff2a0463d9b23ca6d4996512de74f2b258
ChakkritGit <nongtonnee@gmail.com>
2025-08-25 12:57:56 +0700

    update

--- src/validators/inventory.validator.ts
+++ src/validators/inventory.validator.ts
@@ -45,12 +45,12 @@ export const EditInventorySchema = z.object({
 })
 
 export const UpdateStockSchema = z.object({
-  quantity: z.number().min(1).max(60).optional(),
+  quantity: z.number().min(1).max(99).optional(),
   machineId: z
     .string()
     .regex(customMachineIdRegex, { message: 'Invalid Machine ID format.' })
     .optional(),
-    command: z.string().optional()
+  command: z.string().optional()
 })
 
 export type InventoryIdParamsRequestBody = z.infer<
